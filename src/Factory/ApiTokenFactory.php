e348ba33736ba219a8febd7df605e1339f215007
dmitry.kushneriov <dmitry.kushneriov@obreey-products.com>
2025-02-17 14:07:03 +0000

    Debug ApiTokensFactory

--- src/Factory/ApiTokenFactory.php
+++ src/Factory/ApiTokenFactory.php
@@ -12,25 +12,11 @@ use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
  */
 final class ApiTokenFactory extends PersistentProxyObjectFactory
 {
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
-     *
-     * @todo inject services if required
-     */
-    public function __construct()
-    {
-    }
-
     public static function class(): string
     {
         return ApiToken::class;
     }
 
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
-     *
-     * @todo add your default values here
-     */
     protected function defaults(): array|callable
     {
         $monthsCount = self::faker()->numberBetween(0, 12);
@@ -43,15 +29,5 @@ final class ApiTokenFactory extends PersistentProxyObjectFactory
                 ApiToken::SCOPE_TREASURE_EDIT,
             ],
         ];
-    }
-
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
-     */
-    protected function initialize(): static
-    {
-        return $this
-            // ->afterInstantiate(function(ApiToken $apiToken): void {})
-        ;
     }
 }
