25b20a910f44607b701aef66ecaf4518ffb3c1ce
dmitry.kushneriov <dmitry.kushneriov@obreey-products.com>
2025-02-18 00:58:05 +0000

    * Install the "mtdowling/jmespath.php" library * Create test for "GET /api/treasures" request

--- tests/Functional/DragonTreasureResourceTest.php
+++ tests/Functional/DragonTreasureResourceTest.php
@@ -2,26 +2,47 @@
 
 namespace App\Tests\Functional;
 
+use App\Factory\DragonTreasureFactory;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Zenstruck\Browser\Test\HasBrowser;
 use Zenstruck\Foundry\Test\ResetDatabase;
+use Zenstruck\Foundry\Test\Factories;
 
 /**
- * Run tests: ./bin/phpunit DragonTreasureResourceTest
+ * Run tests: ./bin/phpunit tests/Functional/DragonTreasureResourceTest.php
  */
 class DragonTreasureResourceTest extends KernelTestCase
 {
     use HasBrowser;
     use ResetDatabase;
+    use Factories;
 
     private string $baseUrl = '/api';
 
 
     public function testGetCollectionOfTreasures()
     {
-        $this->browser()
+        DragonTreasureFactory::createMany(5);
+
+        $json = $this->browser()
             ->get("{$this->baseUrl}/treasures")
             ->assertStatus(200)
-            ->assertJson();
+            ->assertJson()
+            ->json()
+        ;
+
+        $json
+            ->assertMatches('totalItems', 5)
+            ->assertMatches('keys(member[0])', [
+            '@id',
+            '@type',
+            'name',
+            'description',
+            'value',
+            'coolFactor',
+            'owner',
+            'shortDescription',
+            'plunderedAtAgo',
+        ]);
     }
 }
