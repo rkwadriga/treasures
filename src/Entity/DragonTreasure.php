daf68484ece66cf5aa1d79203dbb10e0418124fe
rkwadriga <remkwadriga2013@gmail.com>
2025-02-21 16:37:59 +0200

    * Make class App\Entity\DragonTreasure to be just an ORM Entity * Delete App\ApiPlatform\AminGroupsContextBuilder * Delete \App\Normalizer\AddOwnerGroupNormalizer * Delete App\State\DragonTreasureStateProcessor * Delete App\State\DragonTreasureStateProvider

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -2,74 +2,15 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Doctrine\Common\Filter\SearchFilterInterface;
-use ApiPlatform\Doctrine\Orm\Filter as Filters;
-use ApiPlatform\Metadata;
-use ApiPlatform\Serializer\Filter\PropertyFilter;
 use App\Repository\DragonTreasureRepository;
-use App\State\DragonTreasureStateProcessor;
-use App\State\DragonTreasureStateProvider;
-use App\Validator\IsValidOwner;
 use Carbon\Carbon;
 use DateTimeImmutable;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use LogicException;
-use Symfony\Component\Serializer\Annotation\Groups;
-use Symfony\Component\Serializer\Annotation\SerializedName;
-use Symfony\Component\Validator\Constraints as Assert;
 use function Symfony\Component\String\u;
 
 #[ORM\Entity(repositoryClass: DragonTreasureRepository::class)]
-#[Metadata\ApiResource(
-    shortName: 'Treasure',
-    description: 'A rare and valuable treasure',
-    operations: [
-        new Metadata\GetCollection(), // Look for App\ApiPlatform\DragonTreasureIsPublishedExtension
-        new Metadata\Get( // Look for App\ApiPlatform\DragonTreasureIsPublishedExtension
-            normalizationContext: [
-                'groups' => ['treasure:read', 'treasure:item:get'],
-            ],
-        ),
-        new Metadata\Post(
-            security: 'is_granted("ROLE_TREASURE_CREATE")',
-            processor: DragonTreasureStateProcessor::class
-        ),
-        new Metadata\Put(
-            security: 'is_granted("ROLE_ADMIN") or (is_granted("ROLE_TREASURE_EDIT") and object.getOwner() == user)',
-            securityPostDenormalize: 'is_granted("ROLE_ADMIN") or object.getOwner() == user',
-            processor: DragonTreasureStateProcessor::class
-        ),
-        new Metadata\Patch(
-            security: 'is_granted("EDIT", object)', // Look for App\Security\Voter\DragonTreasureVoter
-            //securityPostDenormalize: 'is_granted("EDIT", object)' // Look for App\Security\Voter\DragonTreasureVoter
-            processor: DragonTreasureStateProcessor::class
-        ),
-        new Metadata\Delete(security: 'is_granted("ROLE_ADMIN")'),
-    ],
-    formats: ['jsonld', 'json', 'csv' => 'text/csv'], // jsonld and json described in config\packages\api_platform.yaml, csv added
-    normalizationContext: [
-        'groups' => ['treasure:read'],
-    ],
-    denormalizationContext: [
-        'groups' => ['treasure:write'],
-    ],
-    paginationItemsPerPage: 10,
-    provider: DragonTreasureStateProvider::class
-)]
-#[Metadata\ApiResource( // Allows to select all treasures of specific user
-    uriTemplate: '/users/{user_id}/treasures.{_format}',
-    shortName: 'Treasure',
-    operations: [new Metadata\GetCollection()],
-    uriVariables: [
-        'user_id' => new Metadata\Link(fromProperty: 'dragonTreasures', fromClass: User::class, description: 'User identifier'),
-    ],
-    normalizationContext: [
-        'groups' => ['treasure:read'],
-    ],
-)]
-#[Metadata\ApiFilter(PropertyFilter::class)] // Allows to get only selected fields
-#[Metadata\ApiFilter(Filters\SearchFilter::class, properties: ['owner.username' => SearchFilterInterface::STRATEGY_PARTIAL])] // Allows to filter entities by owners (Like GET /treasures?owner.username=<part_of_username>)
 class DragonTreasure
 {
     #[ORM\Id]
@@ -78,50 +19,25 @@ class DragonTreasure
     private ?int $id;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['treasure:read', 'treasure:write', 'user:read', 'user:write'])] // Group "user:item:get" needed to show this attribute only in "GET /users/<id>" request, "user:write" - for writing this attribute in "PATCH /users/<id>" request
-    #[Metadata\ApiFilter(Filters\SearchFilter::class, strategy: SearchFilterInterface::STRATEGY_PARTIAL)]
-    #[Assert\NotBlank]
-    #[Assert\Length(min: 2, max: 255, maxMessage: 'Describe your loot in 255 characters or less.')]
     private ?string $name;
 
     #[ORM\Column(type: Types::TEXT)]
-    #[Groups('treasure:read')]
-    #[Metadata\ApiFilter(Filters\SearchFilter::class, strategy: SearchFilterInterface::STRATEGY_PARTIAL)]
-    #[Assert\NotBlank]
     private ?string $description = null;
 
     #[ORM\Column]
-    #[Metadata\ApiProperty(description: 'The estimated value of this treasure, in gold coins')]
-    #[Groups(['treasure:read', 'treasure:write', 'user:read', 'user:write'])]// Group "user:item:get" needed to show this attribute only in "GET /user/<id>" request, "user:write" - for writing this attribute in "PATCH /users/<id>" request
-    #[Metadata\ApiFilter(Filters\RangeFilter::class)]
-    #[Assert\GreaterThanOrEqual(0)]
     private int $value = 0;
 
     #[ORM\Column]
-    #[Groups(['treasure:read', 'treasure:write'])]
-    #[Assert\GreaterThanOrEqual(0)]
-    #[Assert\LessThanOrEqual(10)]
     private int $coolFactor = 0;
 
     #[ORM\Column]
     private DateTimeImmutable $plunderedAt;
 
     #[ORM\Column]
-    #[Metadata\ApiFilter(Filters\BooleanFilter::class)]
-    // #[Groups(['treasure:read', 'treasure:write'])]
-    //#[Metadata\ApiProperty(security: 'is_granted("EDIT", object)')] // Look for App\Security\Voter\DragonTreasureVoter
-    // Look for App\ApiPlatform\AminGroupsContextBuilder (it adds for admin the "admin:read" group on serializing and "admin:write" group on deserializing)
-    // Look for App\Normalizer\AddOwnerGroupNormalizer (it adds the "owner:read" group for the Treasure's owner)
-    #[Groups(['admin:read', 'admin:write', 'owner:read', 'treasure:write'])]
     private bool $isPublished;
 
     #[ORM\ManyToOne(inversedBy: 'dragonTreasures')]
     #[ORM\JoinColumn(nullable: false)]
-    #[Groups(['treasure:read', 'treasure:write'])]
-    // #[Assert\NotBlank] // Look for App\State\DragonTreasureStateProcessor
-    #[Assert\Valid] // It's needed for use User validation on updating user in request "PATCH /treasures/<id>" request
-    #[IsValidOwner]
-    #[Metadata\ApiFilter(Filters\SearchFilter::class, strategy: SearchFilterInterface::STRATEGY_EXACT)] // Allows to filter entities by owners (Like GET /treasures?owner=/api/users/<user_id>)
     private ?User $owner = null;
 
     private ?bool $isOwnedByAuthenticatedUser = null;
@@ -155,15 +71,11 @@ class DragonTreasure
         return $this;
     }
 
-    #[Groups('treasure:read')]
     public function getShortDescription(): ?string
     {
         return u($this->description)->truncate(40, '...');
     }
 
-    #[Metadata\ApiProperty(description: 'Set multi-lined treasure description')]
-    #[Groups(['treasure:write', 'user:write'])] // Group "user:write" needed for writing this attribute in "PATCH /users/<id>" request
-    #[SerializedName('description')]
     public function setTextDescription(string $description): static
     {
         $this->description = nl2br($description);
@@ -207,8 +119,6 @@ class DragonTreasure
         return $this;
     }
 
-    #[Metadata\ApiProperty(description: 'A human-readable representation of when this treasure was plundered')]
-    #[Groups('treasure:read')]
     public function getPlunderedAtAgo(): string
     {
         return Carbon::instance($this->getPlunderedAt())->diffForHumans();
@@ -238,8 +148,6 @@ class DragonTreasure
         return $this;
     }
 
-    #[Groups('treasure:read')]
-    #[SerializedName('isMine')]
     public function getIsOwnedByAuthenticatedUser(): ?bool
     {
         if ($this->isOwnedByAuthenticatedUser === null) {
